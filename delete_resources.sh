99dcf0ceac6bbab18d31ef31e52e21c78025e425
EC2 Default User <ec2-user@ip-172-31-11-54.ap-southeast-1.compute.internal>
2019-03-15 10:03:59 +0000

    added nuke

--- delete_resources.sh
+++ delete_resources.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-i = 2
+export i=2
 #for i in {2..26}
 #do
-  sed -i s/yudho-workshops/yudho-workshops$i/g delete_resources.sh
+  sed -i s/yudho-workshops2/yudho-workshops2$i/g delete_resources.sh
   aws-nuke -c nuke-config.yml --profile workshop$i
 #done
