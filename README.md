b91d7dd59f876c22f14a350f22f1e91ed7353acf
EC2 Default User <ec2-user@ip-172-31-11-54.ap-southeast-1.compute.internal>
2019-03-16 03:06:41 +0000

    fixed delete resources

---
+++ README.md
@@ -0,0 +1,2 @@
+This program is to manage AWS accounts used for workshops attendees.
+The program involves creating AWS accounts, creating users, deleting users, and deleting resources 
--- delete_resources.sh
+++ delete_resources.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-export i=2
-#for i in {2..26}
-#do
-  sed -i s/yudho-workshops2/yudho-workshops2$i/g delete_resources.sh
-  aws-nuke -c nuke-config.yml --profile workshop$i
-#done
+
+for i in {2..26}
+do
+  export ACCOUNT_ID=$(aws sts get-caller-identity --profile workshop$i | jq -r '.Account')
+
+  sed -i s/yudho-workshop/yudho-workshop$i/g nuke-config.yml
+  sed -i s/000000000000/$ACCOUNT_ID/g nuke-config.yml
+  
+  printf "yudho-workshop$i\nyudho-workshop$i\n" | ./aws-nuke -c nuke-config.yml --profile workshop$i --no-dry-run
+  
+  sleep 3m
+  
+  sed -i s/yudho-workshop$i/yudho-workshop/g nuke-config.yml
+  sed -i s/$ACCOUNT_ID/000000000000/g nuke-config.yml
+done
