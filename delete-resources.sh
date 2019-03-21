25eedf4ba7519ae5bb816a319f433030b353f8f5
EC2 Default User <ec2-user@ip-172-31-11-54.ap-southeast-1.compute.internal>
2019-03-21 04:14:45 +0000

    reduce sleeptime for delete resources

--- delete-resources.sh
+++ delete-resources.sh
@@ -24,14 +24,12 @@ for i in $(seq $ACCOUNT_START_NUMBER $ACCOUNT_FINISH_NUMBER)
 do
   ACCOUNT_ID=$(aws sts get-caller-identity --profile $PROFILE_PREFIX$i | jq -r '.Account')
 
-  #sed -i s/ROLE_NAME_HERE/$ACCOUNT_NAME_PREFIX$i/g nuke-config.yml
   sed -i s/000000000000/$ACCOUNT_ID/g nuke-config.yml
   
   aws-nuke -c nuke-config.yml  --no-dry-run --force --profile $PROFILE_PREFIX$i
   
-  sleep 1m
+  sleep 5s
   
-  #sed -i s/$ACCOUNT_NAME_PREFIX$i/ROLE_NAME_HERE/g nuke-config.yml
   sed -i s/$ACCOUNT_ID/000000000000/g nuke-config.yml
   echo "Finished deleting resources for account $ACCOUNT_NAME_PREFIX$i"
 done
